5b65951e4cc1478d17e2401fd0dcbebf59f8c69d
aymarc <aymarchristian@yahoo.com>
2026-05-12 07:28:44 +0100

    adds note on Scope and Hosting

--- class1/exercice1.js
+++ class1/exercice1.js
@@ -19,11 +19,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter a number: ', (answer) => {
-  const number = parseInt(answer);
-  isEvenorOdd(number);  
-  rl.close();
-});
+
 
 //console.log(isEvenorOdd(2));
 
@@ -38,3 +34,37 @@ if(isGlow){
         }
     }
 }
+
+
+rl.question('Enter a number: ', (answer) => {
+    const number = parseInt(answer);
+    isEvenorOdd(number);  
+    rl.close();
+  });
+
+
+
+  /**
+   * Note: Scope is the set of rules that determines the visibility(availability) of variables in different parts of the code.
+   * There are three types of scope in JavaScript:
+   * 1. Global scope
+   * 2. Function scope
+   * 3. Block scope
+   * 
+   * Global scope: Variables declared outside any function or block are in the global scope.
+   *              They are accessible throughout the program.
+   * Function scope: Variables declared inside a function are in the function scope.
+   *                 They are accessible only within the function.
+   * Block scope: Variables declared inside a block are in the block scope.
+   *              They are accessible only within the block.
+   * 
+   * 
+   * Note: Hoisting is the process of moving the declaration of variables to the top of the scope.
+   *       It is done automatically by the JavaScript engine.
+   *       It is done only for var declarations, not for let and const declarations.
+   *       It is done only for function declarations, not for function expressions.
+   *       It is done only for class declarations, not for class expressions.
+   *       It is done only for import declarations, not for import expressions.
+   *       It is done only for export declarations, not for export expressions.
+   *       It is done only for import declarations, not for import expressions.
+   * */
